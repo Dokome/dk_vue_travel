4b053a5b4a61e7f562775c274da97296d26578ac
Dokome <1098265675@qq.com>
2021-02-25 11:32:04 +0800

    city_list

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul class="list">
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "CityAlphabet"
+};
+</script>
+<style scoped lang="stylus">
+@import '~styles/varibles.styl'
+.list
+  font-size: .3rem
+  line-height: .4rem
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+  position:absolute
+  top: 1.60rem
+  right: 0
+  bottom: 0
+  width: 0.4rem
+  color: $bgColor
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title">当前城市</div>
+        <div class="button-list">
+          <div class="buttonWrapper">
+            <div class="button">赣州</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">热门城市</div>
+        <div class="button-list">
+          <div class="buttonWrapper">
+            <div class="button">赣州</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">A</div>
+        <div class="item-list">
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">A</div>
+        <div class="item-list">
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">A</div>
+        <div class="item-list">
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import BScroll from "better-scroll";
+export default {
+  name: "CityList",
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper);
+  }
+};
+</script>
+<style scoped lang="stylus">
+@import '~styles/varibles.styl'
+.list
+  position: absolute
+  top: 1.60rem
+  left: 0
+  right:0
+  bottom: 0
+  overflow: hidden
+.title
+  font-size .26rem
+  height: .44rem
+  line-height: .44rem
+  background-color: #eee
+  padding-left: .2rem
+  color: #666
+  border-bottom: .001rem solid #ccc
+.button-list
+  overflow: hidden
+  padding .1rem
+  padding-right .6rem
+  .buttonWrapper
+    box-sizing: border-box
+    float: left
+    width: 33.33%
+    .button
+      text-align: center
+      font-size: .4rem
+      padding .1rem
+      margin: .1rem
+      border .03rem solid $bgColor
+      color: $bgColor
+      border-radius: .1rem
+.item-list
+  .item
+    border-bottom #ccc solid .0005rem
+    font-size: .2rem
+    line-height: .54rem
+    color: #666
+    padding: .1rem
+</style>
